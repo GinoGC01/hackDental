b6e9e217ee373e0dd3f2c25b614f1d899a1b06f3
Gino Ciancia <ginociancia10@gmail.com>
2026-01-09 12:14:44 -0300

    update styles and connect to back

---
+++ src/api/api.routes.js
@@ -0,0 +1,4 @@
+export const apiRoutes = {
+    sendDataTest: 'https://hackdental.app.n8n.cloud/webhook-test/3c18748b-5322-4083-a6ff-df9e84b60cb7',
+    sendData: 'https://hackdental.app.n8n.cloud/webhook/3c18748b-5322-4083-a6ff-df9e84b60cb7',
+};
---
+++ src/api/service.js
@@ -0,0 +1,21 @@
+import { apiRoutes } from './api.routes.js';
+
+export class ApiService {
+    static async sendData(info) { 
+        try{
+            const response = await fetch(apiRoutes.sendDataTest , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(info)
+            });
+
+            const data = await response.json();
+            return data
+        } catch (error) {
+            console.error('Error sending data:', error);
+            throw error;
+        }
+    }
+}
